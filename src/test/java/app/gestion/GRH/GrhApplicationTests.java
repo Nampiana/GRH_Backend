9e0c0aed56c6401cacdaa3c0b43e560a828d2fcc
Nampiana <89507479+Nampiana@users.noreply.github.com>
2025-07-01 10:33:20 +0300

    initialization projet

---
+++ src/test/java/app/gestion/GRH/GrhApplicationTests.java
@@ -0,0 +1,13 @@
+package app.gestion.GRH;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
